262198ebdd96a6cb2c99d2de85a041810b4283b4
mryaomink <koson9satu@gmail.com>
2023-07-11 09:33:31 +0800

    add kategori post from wp

--- lib/pages/news/news_yao.dart
+++ lib/pages/news/news_yao.dart
@@ -14,10 +14,12 @@ class NewsYao extends StatefulWidget {
 
 class _NewsYaoState extends State<NewsYao> {
   List<dynamic> wpPost = [];
+  List<dynamic> featuredPost = [];
   @override
   void initState() {
     super.initState();
     fetchPosts();
+    getFeaturePost();
   }
 
   Future<void> fetchPosts() async {
@@ -32,6 +34,22 @@ class _NewsYaoState extends State<NewsYao> {
         });
       } else {
         throw Exception('Gagal Mendapatkan Artikel');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  Future<void> getFeaturePost() async {
+    try {
+      final response = await get(Uri.parse(
+          'https://mediacenter.banjarbarukota.go.id/wp-json/wp/v2/categories'));
+      if (response.statusCode == 200) {
+        setState(() {
+          featuredPost = jsonDecode(response.body);
+        });
+      } else {
+        throw Exception('Gagal mendapatkan Featured Post');
       }
     } catch (e) {
       print('Error: $e');
@@ -54,27 +72,66 @@ class _NewsYaoState extends State<NewsYao> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: wpPost.length,
-              itemBuilder: (BuildContext context, int index) {
-                final post = wpPost[index];
-                final htmlContent = post['excerpt']['rendered'];
+            SizedBox(
+              child: ListView.builder(
+                primary: false,
+                shrinkWrap: true,
+                itemCount: wpPost.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final post = wpPost[index];
+                  final htmlContent = post['excerpt']['rendered'];
 
-                final postUrl = post['link'];
-                return ListTile(
-                  onTap: () {
-                    _launchUrl(postUrl);
-                  },
-                  title: Text(
-                    post['title']['rendered'],
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                  subtitle: HtmlWidget(
-                    htmlContent,
-                  ),
-                );
-              },
+                  final postUrl = post['link'];
+                  return ListTile(
+                    onTap: () {
+                      _launchUrl(postUrl);
+                    },
+                    title: Text(
+                      post['title']['rendered'],
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    subtitle: HtmlWidget(
+                      htmlContent,
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: Text(
+                'Kategori Berita',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+            SizedBox(
+              height: 400,
+              child: ListView.builder(
+                primary: false,
+                shrinkWrap: true,
+                itemCount: featuredPost.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final post = featuredPost[index];
+
+                  final postUrl = post['link'];
+                  return ListTile(
+                    onTap: () {
+                      _launchUrl(postUrl);
+                    },
+                    title: Text(
+                      post['yoast_head_json']['title'],
+                      style:
+                          Theme.of(context).textTheme.headlineMedium!.copyWith(
+                                fontSize: 14.0,
+                                color: Colors.blue,
+                              ),
+                    ),
+                  );
+                },
+              ),
             ),
           ],
         ),
