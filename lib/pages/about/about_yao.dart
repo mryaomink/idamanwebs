6402a42120f1c6f7993acae1d3b8453694b25257
mryaomink <koson9satu@gmail.com>
2023-07-27 09:19:46 +0800

    add scroll to about page

--- lib/pages/about/about_yao.dart
+++ lib/pages/about/about_yao.dart
@@ -13,114 +13,116 @@ class AboutYao extends StatelessWidget {
       appBar: AppBar(),
       body: Container(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              alignment: Alignment.topCenter,
-              width: 150,
-              height: 150,
-              child: Image.asset(
-                'assets/icon/juara.png',
-                fit: BoxFit.cover,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                alignment: Alignment.topCenter,
+                width: 150,
+                height: 150,
+                child: Image.asset(
+                  'assets/icon/juara.png',
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            const Divider(),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Center(
-              child: MediaQuery.of(context).size.width < 640
+              const Divider(),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Center(
+                child: MediaQuery.of(context).size.width < 640
+                    ? const Text(
+                        'VISI & MISI',
+                        style: TextStyle(
+                            fontSize: 24.0,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      )
+                    : const Text(
+                        'VISI & MISI',
+                        style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+              ),
+              const SizedBox(
+                height: 50.0,
+              ),
+              MediaQuery.of(context).size.width < 640
                   ? const Text(
-                      'VISI & MISI',
+                      'VISI',
                       style: TextStyle(
-                          fontSize: 24.0,
+                          fontSize: 20.0,
                           color: Colors.black,
                           fontWeight: FontWeight.bold),
                     )
                   : const Text(
-                      'VISI & MISI',
+                      'VISI',
                       style: TextStyle(
-                          fontSize: 16.0,
+                          fontSize: 14.0,
                           color: Colors.black,
                           fontWeight: FontWeight.bold),
                     ),
-            ),
-            const SizedBox(
-              height: 50.0,
-            ),
-            MediaQuery.of(context).size.width < 640
-                ? const Text(
-                    'VISI',
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold),
-                  )
-                : const Text(
-                    'VISI',
-                    style: TextStyle(
-                        fontSize: 14.0,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold),
-                  ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            MediaQuery.of(context).size.width < 640
-                ? const Text(
-                    visiTitle,
-                    style: TextStyle(
-                        fontSize: 14.0,
-                        color: Colors.grey,
-                        fontWeight: FontWeight.normal),
-                  )
-                : const Text(
-                    visiTitle,
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.grey,
-                        fontWeight: FontWeight.normal),
-                  ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            MediaQuery.of(context).size.width < 640
-                ? const Text(
-                    'MISI',
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold),
-                  )
-                : const Text(
-                    'MISI',
-                    style: TextStyle(
-                        fontSize: 14.0,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold),
-                  ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            MediaQuery.of(context).size.width < 640
-                ? const Text(
-                    misiTitle,
-                    style: TextStyle(
-                        fontSize: 14.0,
-                        color: Colors.grey,
-                        fontWeight: FontWeight.normal),
-                  )
-                : const Text(
-                    misiTitle,
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.grey,
-                        fontWeight: FontWeight.normal),
-                  ),
-          ],
+              const SizedBox(
+                height: 10.0,
+              ),
+              MediaQuery.of(context).size.width < 640
+                  ? const Text(
+                      visiTitle,
+                      style: TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.normal),
+                    )
+                  : const Text(
+                      visiTitle,
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.normal),
+                    ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              MediaQuery.of(context).size.width < 640
+                  ? const Text(
+                      'MISI',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    )
+                  : const Text(
+                      'MISI',
+                      style: TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              MediaQuery.of(context).size.width < 640
+                  ? const Text(
+                      misiTitle,
+                      style: TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.normal),
+                    )
+                  : const Text(
+                      misiTitle,
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.normal),
+                    ),
+            ],
+          ),
         ),
       ),
     );
